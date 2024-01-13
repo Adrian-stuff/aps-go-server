fb32e1170620dc70ea759c42e01c0329f9477f42
Adrian De Vera <twoadrian6@gmail.com>
2024-01-13 16:48:15 +0800

    x

--- test_gpio.go
+++ test_gpio.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"periph.io/x/conn/v3/gpio"
 	"periph.io/x/host/v3"
@@ -31,44 +30,48 @@ func main() {
 	// 	return
 	// }
 
-	// Main loop
 	for {
-		isReading := true
-		counterTotal := 0
-		total := 0
-		value := 0
-		counter := 0
-
-		for isReading {
-			pinState := pin.Read()
-
-			if pinState == gpio.Low {
-				counter++
-				time.Sleep(100 * time.Millisecond) // Delay for 0.1 seconds
-				fmt.Println("counter: ", counter)
-
-				counterTotal = counter
-				if counterTotal == 1 || counterTotal == 3 || counterTotal == 5 {
-					isReading = false
-				}
-			}
-		}
-
-		if counterTotal == 1 || counterTotal == 3 {
-			value++ // Increment value directly (equivalent to value + 1)
-		} else if counterTotal == 5 {
-			value += 2 // Add 2 for counterTotal == 5
-		}
-
-		total += value
-
-		if total != 0 {
-			fmt.Println("total:", total)
-		}
-
-		// Reset variables for the next cycle
-		total = 0
-		value = 0
-		counter = 0
+		pinState := pin.Read()
+		fmt.Printf("%t \n", pinState)
 	}
+	// Main loop
+	// for {
+	// 	isReading := true
+	// 	counterTotal := 0
+	// 	total := 0
+	// 	value := 0
+	// 	counter := 0
+
+	// 	for isReading {
+	// 		pinState := pin.Read()
+
+	// 		if pinState == gpio.Low {
+	// 			counter++
+	// 			time.Sleep(100 * time.Millisecond) // Delay for 0.1 seconds
+	// 			fmt.Println("counter: ", counter)
+
+	// 			counterTotal = counter
+	// 			if counterTotal == 1 || counterTotal == 3 || counterTotal == 5 {
+	// 				isReading = false
+	// 			}
+	// 		}
+	// 	}
+
+	// 	if counterTotal == 1 || counterTotal == 3 {
+	// 		value++ // Increment value directly (equivalent to value + 1)
+	// 	} else if counterTotal == 5 {
+	// 		value += 2 // Add 2 for counterTotal == 5
+	// 	}
+
+	// 	total += value
+
+	// 	if total != 0 {
+	// 		fmt.Println("total:", total)
+	// 	}
+
+	// 	// Reset variables for the next cycle
+	// 	total = 0
+	// 	value = 0
+	// 	counter = 0
+	// }
 }
